5886b5ad547c72fb89b150bd2b103ddd4353002b
Yovie Fesya Pratama <yoviefp@gmail.com>
2022-03-31 14:44:14 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+import React, { useState, useEffect } from "react";
+import Pagination from "./Pagination";
+import Post from "./Post";
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("something went wrong while requesting posts");
+      })
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) return <h1>{error}</h1>;
+  return (
+    <div className="App">
+      <div>
+        {posts.length > 0 ? (
+          <>
+            <Pagination
+              data={posts}
+              RenderComponent={Post}
+              title="Posts"
+              pageLimit={5}
+              dataLimit={10}
+            />
+          </>
+        ) : (
+          <h1>No Posts to display</h1>
+        )}
+      </div>
+    </div>
+  );
+}
